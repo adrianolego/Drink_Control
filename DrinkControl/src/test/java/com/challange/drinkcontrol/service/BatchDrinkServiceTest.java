80e94047e6c6024165c30bd15c4e1cf51de5539e
adrianolego <adriano__silva__@hotmail.com>
2020-07-01 12:15:20 -0300

    Inicializing test suite

---
+++ DrinkControl/src/test/java/com/challange/drinkcontrol/service/BatchDrinkServiceTest.java
@@ -0,0 +1,70 @@
+package com.challange.drinkcontrol.service;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BatchDrinkServiceTest {
+
+//    @Autowired
+//    private BatchDrinkService batchDrinkService;
+//
+//    @Autowired
+//    private BatchDrinkBuilder batchDrinkBuilder;
+//
+//    private BatchDrinkRepository batchDrinkRepositoryMock;
+
+    @Before
+    public void setup(){
+//         batchDrinkRepositoryMock = mock(BatchDrinkRepository.class);
+    }
+
+    @Test
+    public void teste(){
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void findDrinkBatchById(){
+//        BatchDrink batchDrink = batchDrinkBuilder.now();
+//        Mockito.when(batchDrinkRepositoryMock.findById(any())).thenReturn(Optional.of(batchDrink));
+//
+//        BatchDrink result =  batchDrinkService.find(1);
+//
+//        assertEquals(batchDrink.getId(),result.getId());
+//        assertEquals(batchDrink.getAmount(),result.getAmount());
+//        assertEquals(batchDrink.getDateTime(),result.getDateTime());
+//        assertEquals(batchDrink.getResponsiblePerson(),result.getResponsiblePerson());
+
+        Assert.assertNotEquals(2,2);
+    }
+//
+//    @Test
+//    public void notFindDrinkBatchById(){
+//
+//    }
+//
+//    @Test
+//    public void FindAllDrinkBatches(){
+//
+//    }
+//
+//    @Test
+//    public void InsertDrinkBatch(){
+//
+//    }
+//
+//    @Test
+//    public void UpdateDrinkBatch(){
+//
+//    }
+//
+//    @Test
+//    public void DeleteDrinkBatch(){
+//
+//    }
+
+}
